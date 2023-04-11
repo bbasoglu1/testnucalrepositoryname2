15e26a93133465807b1412104e507ad5f07ff60b
NUCAL <beritan.karali@ceng.deu.edu.tr>
2023-04-11 14:05:57 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,84 @@
+<template>
+  <DropZone ncBgType="bgColor" backgroundImage="" id="68b0de87-b1ec-4cec-88c1-5b3774df569d"
+    v-bind:style="{'width':'100%','height':'auto'}">
+    <WBadge href="" color="#FFFFFF" v-bind:exact="false" v-bind:active="false" v-bind:append="false" caption="Badge"
+      variant="secondary" v-bind:disabled="false" linkTarget="_top" ncLinkType="link" tagOfBadge="a"
+      v-bind:pillOfBadge="false" ncTypography="body1" ncTooltipText=""
+      v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'caption','displayName':'Caption'}]}"
+      toForRouterLink="to" ncClickFontColor="#0000FF" ncFocusFontColor="#00FF00" ncHoverFontColor="#ff0000"
+      ncDisableFontColor="#4285f4" whichHeadTagForSize="h5" position="relative"
+      id="0c92750a-7c86-45b9-ae58-cc396da85b00"
+      v-bind:style="{'width':'100%','height':'100%','tabIndex':'-1','fontStyle':'normal','marginTop':'0px','textAlign':'center','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'1.25','marginLeft':'0px','paddingTop':'5px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'5px','marginBottom':'0px','paddingRight':'5px','paddingBottom':'5px','textDecoration':'none','zIndex':2}"/>
+  </DropZone>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024.05px){[id='68b0de87-b1ec-4cec-88c1-5b3774df569d'] {
+             display: grid; 
+             grid-template-columns: 35.029296875rem 4.296875rem 60.673828125rem;
+             grid-template-rows: minmax(22.16796875rem, max-content) minmax(2.265625rem, max-content) minmax(58.37890625rem, max-content);
+          }
+[id='0c92750a-7c86-45b9-ae58-cc396da85b00']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(15rem, 12px),1.171875rem);
+          
+          }}
+
+@media (min-width: 768.05px) and (max-width: 1024px){[id='68b0de87-b1ec-4cec-88c1-5b3774df569d'] {
+             display: grid; 
+             grid-template-columns: 47.314453125rem 5.37109375rem 47.314453125rem;
+             grid-template-rows: minmax(27.7099609375rem, max-content) minmax(2.83203125rem, max-content) minmax(3.125rem, max-content);
+          }
+[id='0c92750a-7c86-45b9-ae58-cc396da85b00']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(15rem, 12px),1.46484375rem);
+          
+          }}
+
+@media (min-width: 480.05px) and (max-width: 768px){[id='68b0de87-b1ec-4cec-88c1-5b3774df569d'] {
+             display: grid; 
+             grid-template-columns: 46.419270833333336rem 7.161458333333333rem 46.419270833333336rem;
+             grid-template-rows: minmax(36.946614583333336rem, max-content) minmax(3.7760416666666665rem, max-content) minmax(4.166666667rem, max-content);
+          }
+[id='0c92750a-7c86-45b9-ae58-cc396da85b00']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(15rem, 12px),1.953125rem);
+          
+          }}
+
+@media (min-width: 375.05px) and (max-width: 480px){[id='68b0de87-b1ec-4cec-88c1-5b3774df569d'] {
+             display: grid; 
+             grid-template-columns: 44.270833333333336rem 11.458333333333334rem 44.270833333333336rem;
+             grid-template-rows: minmax(59.114583333333336rem, max-content) minmax(6.041666666666667rem, max-content) minmax(5rem, max-content);
+          }
+[id='0c92750a-7c86-45b9-ae58-cc396da85b00']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(15rem, 12px),3.125rem);
+          
+          }}
+
+@media (min-width: 0px) and (max-width: 375px){[id='68b0de87-b1ec-4cec-88c1-5b3774df569d'] {
+             display: grid; 
+             grid-template-columns: 6.4rem 87.2rem 6.4rem;
+             grid-template-rows: minmax(75.66666666666667rem, max-content) minmax(45.978181818181824rem, max-content) minmax(6.4rem, max-content);
+          }
+[id='0c92750a-7c86-45b9-ae58-cc396da85b00']{
+          grid-area: 2/2/3/3;
+          font-size: Max(Min(15rem, 12px),23.781818181818185rem);
+          
+          }}
+</style>
